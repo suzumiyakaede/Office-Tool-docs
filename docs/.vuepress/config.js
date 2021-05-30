6615bcb58babb2bd12881bb3098d41d116066e22
Yerong <44803668+YerongAI@users.noreply.github.com>
2021-05-30 23:22:39 +0800

    Create files for zh-tw.

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -124,6 +124,54 @@ module.exports = {
 						''
 					]
 				}
+			},
+			'/zh-tw/': {
+				selectText: '選擇語言',
+				label: '繁體中文 (台灣)',
+				editLinkText: '在 GitHub 上编辑此页',
+				serviceWorker: {
+					updatePopup: {
+						message: "发现新内容可用.",
+						buttonText: "刷新"
+					}
+				},
+				nav: [
+					{ text: '入门', link: '/zh-tw/start/' },
+					{ text: '部署', link: '/zh-tw/deploy/' },
+					{ text: '激活', link: '/zh-tw/activate/' },
+					{ text: '更多', link: '/zh-tw/others/toolbox' },
+					{ text: '官方博客', link: 'https://www.coolhub.top' },
+					{
+						text: '群组', items: [
+							{ text: 'Telegram', link: 'https://otp.landian.vip/grouplink/telegram.html' }
+						]
+					}
+				],
+				sidebar: {
+					'/zh-tw/start/': [
+						'',
+						'requirement',
+						'download'
+					],
+					'/zh-tw/deploy/': [
+						'',
+						'deploy',
+						'create-iso',
+						'faq'
+					],
+					'/zh-tw/activate/': [
+						'',
+						'faq'
+					],
+					'/zh-tw/others/': [
+						'toolbox',
+						'converter',
+						''
+					],
+					'/zh-tw/': [
+						''
+					]
+				}
 			}
 		}
 	}
---
+++ docs/zh-tw/start/requirement.md
@@ -0,0 +1,21 @@
+# 系统要求
+
+## 框架
+
+[.NET 5 Desktop Runtime x86](https://dotnet.microsoft.com/download/dotnet/current/runtime).
+
+with runtime 版本中自带 Runtime library，你可以通过双击 RunMe.bat 直接打开 Office Tool Plus 而无需安装 Desktop Runtime.
+
+## 支持的系统
+
+Windows 7 SP1, Windows 8, Windows 10.
+
+Windows Server 2008 R2 SP1, Windows Server 2012 或者更高版本。
+
+> Mac 用户请自行处理，Office Tool Plus 不支持也不可能支持 Mac OS.
+
+## 针对 Windows 7 无法打开的解决方案
+
+由于大多数 Windows 7 系统并没有更新，为了能顺利运行 Office Tool Plus，必须更新系统至最新版。
+
+通常情况下可以使用 Windows Update 将系统更新到最新，如果 Windows Update 无法运行或者需要一次性更新多个系统，请[下载 Win 7 专用系统更新包](https://download.coolhub.top/Extensions/Win7_UpdatePack/)使用。
